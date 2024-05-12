2927dc47c7ae2359a5e6fe5012e01bc15df16ab2
Aman Kumar <aman.sinha74458@gmail.com>
2024-05-12 17:42:47 +0530

    feat: Add setResizePane method to splitUtils class
    This commit introduces the setResizePane method in the splitUtils class. This method allows users to resize the pane size, which is restricted to a range between 0 and 100 (excluding 0 and 100). The size is specified in percentage format and will be converted to pixels or remain in percentage format based on the current pane size format.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const openSplitter = SplitUtils.openSplitter.bind(SplitUtils);
 const closeSplitter = SplitUtils.closeSplitter.bind(SplitUtils);
 const getSplitPaneInstance = SplitUtils.getSplitPaneInstance.bind(SplitUtils);
 const isSectionOpen = SplitUtils.isSectionOpen.bind(SplitUtils);
+const setPaneSize = SplitUtils.setPaneSize.bind(SplitUtils);
 const fixClass = SplitUtils.fixClass();
 
-export { Split, openSplitter, closeSplitter, getSplitPaneInstance, isSectionOpen, fixClass, SplitSessionStorage };
+export { Split, openSplitter, closeSplitter, getSplitPaneInstance, isSectionOpen, fixClass, setPaneSize, SplitSessionStorage };
