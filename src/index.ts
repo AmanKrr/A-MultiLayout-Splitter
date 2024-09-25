31a5ad7c190243686a7e643aa976c6490fa9c009
Aman Kumar <aman.sinha74458@gmail.com>
2024-09-25 15:20:17 +0530

    fix: Type fix and context export fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Split from "./base/Split";
 import SplitUtils from "./utils/SplitUtils";
 import SplitSessionStorage from "./utils/SplitSessionStorage";
+import { SplitStateProvider } from "./base/SplitProvider";
 
 const openSplitter = SplitUtils.openSplitter.bind(SplitUtils);
 const closeSplitter = SplitUtils.closeSplitter.bind(SplitUtils);
@@ -9,4 +10,4 @@ const isSectionOpen = SplitUtils.isSectionOpen.bind(SplitUtils);
 const setPaneSize = SplitUtils.setPaneSize.bind(SplitUtils);
 const fixClass = SplitUtils.fixClass();
 
-export { Split, openSplitter, closeSplitter, getSplitPaneInstance, isSectionOpen, fixClass, setPaneSize, SplitSessionStorage };
+export { Split, openSplitter, closeSplitter, getSplitPaneInstance, isSectionOpen, fixClass, setPaneSize, SplitSessionStorage, SplitStateProvider };
