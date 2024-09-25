7dbdeff1a91f3b7f23e3b5963b8f47f27694befa
Aman Kumar <aman.sinha74458@gmail.com>
2024-09-25 15:21:52 +0530

    build: type fixes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,11 @@
 import Split from "./base/Split";
 import SplitUtils from "./utils/SplitUtils";
 import SplitSessionStorage from "./utils/SplitSessionStorage";
+import { SplitStateProvider } from "./base/SplitProvider";
 declare const openSplitter: typeof SplitUtils.openSplitter;
 declare const closeSplitter: typeof SplitUtils.closeSplitter;
 declare const getSplitPaneInstance: typeof SplitUtils.getSplitPaneInstance;
 declare const isSectionOpen: typeof SplitUtils.isSectionOpen;
 declare const setPaneSize: typeof SplitUtils.setPaneSize;
 declare const fixClass: string;
-export { Split, openSplitter, closeSplitter, getSplitPaneInstance, isSectionOpen, fixClass, setPaneSize, SplitSessionStorage };
+export { Split, openSplitter, closeSplitter, getSplitPaneInstance, isSectionOpen, fixClass, setPaneSize, SplitSessionStorage, SplitStateProvider };
