3598c6293030766519e42f48874c333df6895cdc
Aman Kumar <aman.kumar74458@gmail.com>
2026-01-27 23:39:33 +0530

    chore: remove archive legacy v5

--- archive/v5-legacy/packages-core/test/setup.ts
+++
@@ -1,8 +0,0 @@
-import { expect, afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/vitest';
-
-// Cleanup after each test
-afterEach(() => {
-  cleanup();
-});
