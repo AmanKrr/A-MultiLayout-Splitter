7d147609014315966d34b9c4758fdc0a74e38faf
Aman Kumar <aman.kumar74458@gmail.com>
2026-01-28 02:42:44 +0530

    chore: Semantic release setup

---
+++ release.config.mjs
@@ -0,0 +1,27 @@
+export default {
+  branches: [
+    {
+      name: "v6",
+      prerelease: "alpha",
+    },
+    "main",
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/npm",
+      {
+        pkgRoot: "packages/core",
+        npmPublish: false,
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["packages/core/package.json", "packages/core/CHANGELOG.md"],
+      },
+    ],
+  ],
+};
